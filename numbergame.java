3c30a34c19c7b2fb4a2fd9452e876e34a551ab02
Your Namesaifxyzyz <saifmujeeb2603@gmail.com>
2024-05-09 10:47:25 +0530

    first commit

---
+++ numbergame.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+public class generaterandom{
+    public static void main(String args[]){
+        Random rand = new Random();
+        int n1 = rand.nextInt(100);
+        System.out.println(n1);
+    }
+    
+}
